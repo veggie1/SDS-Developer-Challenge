108807cfc52205f1cb739a32a359527ec3e05495
Ben Baacke <ben.baacke@gmail.com>
2024-10-16 18:06:34 -0400

    Container

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private readonly Dictionary<Type, Type> _bindings = new();
+
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            var canAssign = interfaceType.IsAssignableFrom(implementationType);
+
+            if (!canAssign)
+            {
+                throw new InvalidCastException($"{interfaceType} is not assignable to {implementationType}");
+            }
+
+            _bindings.Add(interfaceType, implementationType);
+        }
+
+        public T Get<T>()
+        {
+            if (!_bindings.ContainsKey(typeof(T)))
+            {
+                throw new KeyNotFoundException($"Key {typeof(T)} was not found");
+            }
+
+            var type = _bindings[typeof(T)];
+
+            return (T)Activator.CreateInstance(type);
+        }
     }
 }
--- Container/ContainerTest.cs
+++ Container/ContainerTest.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 
 namespace DeveloperSample.Container
@@ -17,8 +20,26 @@ namespace DeveloperSample.Container
         {
             var container = new Container();
             container.Bind(typeof(IContainerTestInterface), typeof(ContainerTestClass));
+
             var testInstance = container.Get<IContainerTestInterface>();
+
             Assert.IsType<ContainerTestClass>(testInstance);
+        }
+
+        [Fact]
+        public void IncorrectBindingThrowsException()
+        {
+            var container = new Container();
+            
+            Assert.Throws<InvalidCastException>(() => container.Bind(typeof(IList), typeof(ContainerTestClass)));
+        }
+
+        [Fact]
+        public void GetUnboundTypeThrowsKeyNotFoundException()
+        {
+            var container = new Container();
+
+            Assert.Throws<KeyNotFoundException>(container.Get<IContainerTestInterface>);
         }
     }
 }
